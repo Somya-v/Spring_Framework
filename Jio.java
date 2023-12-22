905f3d8a7988e6b7e35c07fa8cea2a42be31e8a6
Somya Vishnoi <54271946+Somya-v@users.noreply.github.com>
2023-12-22 14:04:46 +0530

    Jio sim that implements sim interface

---
+++ Jio.java
@@ -0,0 +1,13 @@
+package org.somya.SprinDemo.simExample;
+
+public class Jio implements Sim {
+
+	@Override
+	public void calling() {
+		System.out.println("calling using Jio sim");
+	}
+	@Override
+	public void data() {
+		System.out.println("browsing internet using Jio sim");
+	}
+}
